23bfd6b259c4800f0de2ebf99ade352f5d885f66
TheBlupper <33233478+TheBlupper@users.noreply.github.com>
2021-08-04 13:29:06 +0200

    Added solution.py

--- uiuctf2021/Constructive Criticism/solution.py
+++ uiuctf2021/Constructive Criticism/solution.py
@@ -1,73 +1,17 @@
-from scipy.io import wavfile
-import os
-import numpy as np
+import angr
 
+base_addr = 0x00100000
+find_addr = 0x001019d1
+avoid_addr = 0x001019a9
 
-SONG_DIR = './tracks/'
-MARGIN = 2000
+proj = angr.Project('./challenge', main_opts= {'base_addr' : base_addr})
 
-MINIMUM_HEIGHT = 10
+simgr = proj.factory.simulation_manager()
+simgr.explore(find=find_addr, avoid=avoid_addr)
 
-def main():
-    result = ''
-
-    # Loop through all the files in the specified directory
-    for file in os.listdir(SONG_DIR):
-        # We only care about the sound files
-        if not file.endswith('.wav'):
-            continue
-
-        filepath = os.path.join(SONG_DIR, file)
-
-        samplerate, data = wavfile.read(filepath)
-
-        # Unpack the data and separate the channels
-        sample_num, channel_num = data.shape
-        c1, c2, _ = data.T
-
-        print(f'[*] Comparing channel 1 & 2 in {file}')
-
-        # Find the difference between the first two channels
-        sub = c1 - c2
-        
-        byte_num = 3
-        while True:
-            
-            # Samples/bit
-            bit_size = sample_num // (byte_num * 8)
-
-            text = ''
-            for by in range(byte_num): # For each byte in the song
-
-                # bits will be string of 8 1s and 0s, which is later converted to a byte
-                bits = ''
-
-                for bi in range(8): # For each bit in the byte
-
-                    i = by * 8 + bi # Compounded index of bit
-
-                    arr = sub[i * bit_size + MARGIN : (i + 1) * bit_size - MARGIN]
-                    m = np.amax(arr) # Find the highest point in the area
-                    
-                    # Check wheather the highest point is tall enough or not
-                    bit = '1' if m > MINIMUM_HEIGHT else '0' 
-                    bits += bit
-
-                # Use string->int conversion with base 2, and make a character out of the bits
-                text += chr(int(bits, 2))
-
-            # Some (the last) files use 6 bytes instead of 3, so if the data
-            # seems corrupted, we add 3 bytes and run extraction again
-            if not text.isascii():
-                byte_num += 3
-                print(f'[!] {text} is not ASCII! Trying with {byte_num} bits')
-                continue
-            break
-        result += text
-
-        print(f'[*] Current string: "{result}"')
-
-    print(f'\nFLAG: {result}\n')
-
-if __name__ == '__main__':
-    main()
+STDIN_FD = 0 # 0 is the file descriptor of STDIN
+if len(simgr.found) > 0:
+    for found in simgr.found:
+        print(f'Found flag: ' + found.posix.dumps(STDIN_FD).decode())
+else:
+    print('Could not find flag :(')
